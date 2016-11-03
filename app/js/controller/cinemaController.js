8ad3cb8a2abf265d61246286165011f34d1e1251
Thibaud Crespin <thibaud.crespin@biig.fr>
2016-11-03 15:09:46 +0100

    modified cinemaController

---
+++ app/js/controller/cinemaController.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* Controllers */
+
+var projet = angular.module('projet');
+
+projet.controller('CinemaController', ['$scope','CinemaService','$filter', function ( $scope, CinemaService, $filter ) {
+
+
+    CinemaService.listCinema().then(function(response){
+        $scope.items = response.data;
+    });
+
+    $scope.map = { center: { latitude: 46.1577, longitude: -1.15359 }, zoom: 8 };
+
+}]);
